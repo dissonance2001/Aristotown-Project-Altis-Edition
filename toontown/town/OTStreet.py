49de34902a0ca4082ff8d145635582a349a0ab7b
DankMickey <jareddarty96@gmail.com>
2025-07-26 00:52:02 -0400

    town: Add ambient crow/ raven sounds in Ye Olde streets

--- toontown/town/OTStreet.py
+++ toontown/town/OTStreet.py
@@ -15,7 +15,30 @@ class OTStreet(Street.Street):
         Street.Street.unload(self)
 
     def enter(self, requestStatus):
+        self.nextBirdTime = 0
+        taskMgr.add(self.__birds, 'OT-birds')
         Street.Street.enter(self, requestStatus)
 
     def exit(self):
+        taskMgr.remove('OT-birds')
         Street.Street.exit(self)
+
+    def __birds(self, task):
+        if task.time < self.nextBirdTime:
+            return Task.cont
+        randNum = random.random()
+        bird = int(randNum * 100) % 6 + 1
+        if bird == 1:
+            base.playSfx(self.loader.crow1Sound)
+        elif bird == 2:
+            base.playSfx(self.loader.crow2Sound)
+        elif bird == 3:
+            base.playSfx(self.loader.raven1Sound)
+        elif bird == 4:
+            base.playSfx(self.loader.raven2Sound)
+        elif bird == 5:
+            base.playSfx(self.loader.ravenCrow1Sound)
+        elif bird == 6:
+            base.playSfx(self.loader.ravenCrow2Sound)
+        self.nextBirdTime = task.time + randNum * 40.0
+        return Task.cont
--- toontown/town/OTTownLoader.py
+++ toontown/town/OTTownLoader.py
@@ -14,12 +14,24 @@ class OTTownLoader(TownLoader.TownLoader):
 
     def load(self, zoneId):
         TownLoader.TownLoader.load(self, zoneId)
+        self.crow1Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_c1.ogg')
+        self.crow2Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_c2.ogg')
+        self.raven1Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_r1.ogg')
+        self.raven2Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_r2.ogg')
+        self.ravenCrow1Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_rc1.ogg')
+        self.ravenCrow2Sound = base.loader.loadSfx('phase_7/audio/sfx/yott_c2.ogg')
         Suit.loadSuits(3)
         dnaFile = 'phase_7/dna/olde_toontown_' + str(self.canonicalBranchZone) + '.dna'
         self.createHood(dnaFile)
 
     def unload(self):
         TownLoader.TownLoader.unload(self)
+        del self.crow1Sound
+        del self.crow2Sound
+        del self.raven1Sound
+        del self.raven2Sound
+        del self.ravenCrow1Sound
+        del self.ravenCrow2Sound
         Suit.unloadSuits(3)
         
     def enter(self, requestStatus):
