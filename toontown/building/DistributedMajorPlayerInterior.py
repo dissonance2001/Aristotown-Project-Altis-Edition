db3300544542f83875b0cc96eb3e1032fe7b5d09
Reia <microwavex7@gmail.com>
2026-07-20 18:19:14 +0200

    Fixed Major Player's building door + music

--- toontown/building/DistributedMajorPlayerInterior.py
+++ toontown/building/DistributedMajorPlayerInterior.py
@@ -4,59 +4,119 @@ from pandac.PandaModules import *
 from direct.interval.IntervalGlobal import *
 from direct.distributed.ClockDelta import *
 from toontown.toonbase import ToontownGlobals
-import ToonInterior
 from DistributedToonInterior import DistributedToonInterior
-import ToonInteriorColors, random
-from direct.directnotify import DirectNotifyGlobal
-from direct.distributed import DistributedObject
+import ToonInteriorColors
 import random
 from toontown.hood import ZoneUtil
-from toontown.char import Char
-from toontown.quest import QuestParser
 from toontown.dna.DNAParser import DNADoor
-from toontown.hood import ZoneUtil
 
 class DistributedMajorPlayerInterior(DistributedToonInterior):
 
     def __init__(self, cr):
         DistributedToonInterior.__init__(self, cr)
+        self.majorPlayerMusic = None
+        self.majorPlayerMusicFile = None
+        self.majorPlayerMusicTaskName = 'majorPlayerLobbyMusic-%s' % id(self)
 
     def setup(self):
         self.dnaStore = base.cr.playGame.dnaStore
         self.randomGenerator = random.Random()
         self.randomGenerator.seed(self.zoneId)
+
         interior = self.randomDNAItem('TI_dave', self.dnaStore.findNode)
         self.interior = interior.copyTo(render)
-        self.interior.setHpr(180, 0, 0) # doesn't flip dave interior
+
         hoodId = ZoneUtil.getCanonicalHoodId(self.zoneId)
         self.colors = ToonInteriorColors.colors[hoodId]
         self.replaceRandomInModel(self.interior)
+
         doorModelName = 'door_double_round_ul'
         if doorModelName[-1:] == 'r':
             doorModelName = doorModelName[:-1] + 'l'
         else:
             doorModelName = doorModelName[:-1] + 'r'
+
         door = self.dnaStore.findNode(doorModelName)
-        door_origin = render.find('**/door_origin;+s')
-        doorNP = door.copyTo(door_origin)
-        door_origin.setScale(0.8, 0.8, 0.8)
-        door_origin.setPos(door_origin, 0, -0.025, 0)
+
+        originalDoorOrigin = self.interior.find('**/door_origin;+s')
+        originalDoorTransform = originalDoorOrigin.getTransform(self.interior)
+        originalDoorOrigin.setName('original_door_origin')
+
+        visualDoorRoot = self.interior.attachNewNode('major_player_visual_door_root')
+        visualDoorRoot.setPos(self.interior, -1.2, 0.152, 0.02)
+        visualDoorRoot.setHpr(self.interior, 180, 0, 0)
+        visualDoorRoot.setScale(0.8, 0.8, 0.8)
+
+        visualDoorOrigin = visualDoorRoot.attachNewNode('major_player_visual_door_origin')
+        doorNP = door.copyTo(visualDoorOrigin)
+
         color = self.randomGenerator.choice(self.colors['TI_door'])
-        DNADoor.setupDoor(doorNP, self.interior, door_origin, self.dnaStore, str(self.block), color)
+        DNADoor.setupDoor(
+            doorNP,
+            visualDoorRoot,
+            visualDoorOrigin,
+            self.dnaStore,
+            str(self.block),
+            color
+        )
+
         doorFrame = doorNP.find('door_*_flat')
-        doorFrame.wrtReparentTo(self.interior)
+        doorFrame.wrtReparentTo(visualDoorRoot)
         doorFrame.setColor(color)
+
         elevator = loader.loadModel('phase_11/models/lawbotHQ/lawbotElevator.bam')
-        elevator_origin = render.find('**/elevator_origin')
-        elevator.reparentTo(elevator_origin)
+        elevatorOrigin = self.interior.find('**/elevator_origin')
+        elevator.reparentTo(elevatorOrigin)
+
         del self.colors
         del self.dnaStore
         del self.randomGenerator
-        self.interior.flattenMedium()
+
+        spawnDoorOrigin = self.interior.attachNewNode('door_origin')
+        spawnDoorOrigin.setTransform(self.interior, originalDoorTransform)
+        spawnDoorOrigin.setH(spawnDoorOrigin, 180)
+        spawnDoorOrigin.setY(spawnDoorOrigin, 0.5)
+
+        taskMgr.doMethodLater(
+            0.1,
+            self.__startMajorPlayerMusic,
+            self.majorPlayerMusicTaskName
+        )
+
+    def __startMajorPlayerMusic(self, task):
+        base.musicManager.stopAllSounds()
+        self.majorPlayerMusicFile = loader.loadMusic(
+            'phase_12/audio/bgm/merc/instance_majorplayer_lobby.ogg'
+        )
+        self.majorPlayerMusic = base.playMusic(
+            self.majorPlayerMusicFile,
+            looping=1
+        )
+        return task.done
+
+    def __stopMajorPlayerMusic(self):
+        taskMgr.remove(self.majorPlayerMusicTaskName)
+        base.musicManager.stopAllSounds()
+
+        if self.majorPlayerMusic:
+            try:
+                self.majorPlayerMusic.stop()
+            except:
+                pass
+            self.majorPlayerMusic = None
+
+        if self.majorPlayerMusicFile:
+            try:
+                self.majorPlayerMusicFile.stop()
+            except:
+                pass
+            self.majorPlayerMusicFile = None
 
     def disable(self):
+        self.__stopMajorPlayerMusic()
         self.enterOff()
         DistributedToonInterior.disable(self)
 
     def delete(self):
+        self.__stopMajorPlayerMusic()
         DistributedToonInterior.delete(self)
