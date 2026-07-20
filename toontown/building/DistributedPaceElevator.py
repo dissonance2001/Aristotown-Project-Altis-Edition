ad38587e459134c734d0d761bc16626a6426c3f5
Reia <microwavex7@gmail.com>
2026-07-20 02:33:30 +0200

    Pace Elevator Ride Movie

--- toontown/building/DistributedPaceElevator.py
+++ toontown/building/DistributedPaceElevator.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from direct.interval.IntervalGlobal import Func, Sequence, SoundInterval
+from pandac.PandaModules import Point3
+from direct.interval.IntervalGlobal import Func, Sequence, SoundInterval, Wait, LerpPosInterval
 from toontown.building import DistributedElevator
 from toontown.building import DistributedBossElevator
 from toontown.building.ElevatorConstants import *
@@ -8,6 +9,8 @@ from toontown.toonbase import TTLocalizer
 
 
 class DistributedPaceElevator(DistributedBossElevator.DistributedBossElevator):
+
+    RideDuration = 8.0
 
     def __init__(self, cr):
         DistributedBossElevator.DistributedBossElevator.__init__(self, cr)
@@ -28,9 +31,19 @@ class DistributedPaceElevator(DistributedBossElevator.DistributedBossElevator):
         self.clunkSfx = base.loader.loadSfx(
             'phase_5/audio/sfx/cogbldg_door_close.ogg'
         )
+        self.rideMusic = base.loader.loadMusic(
+            'phase_9/audio/bgm/merc/instance_pacesetter_elevator.ogg'
+        )
 
         self.finalOpenSfx = None
         self.finalCloseSfx = None
+        self.rideTrack = None
+        self.rideHiddenGeom = None
+        self.previousMinFov = None
+        self.rideElevatorStartPos = None
+        self.rideRoot = None
+        self.rideOriginalParent = None
+        self.rideOriginalMat = None
 
     def setupElevator(self):
         self.elevatorModel = loader.loadModel(
@@ -55,10 +68,6 @@ class DistributedPaceElevator(DistributedBossElevator.DistributedBossElevator):
             locator = render.find('**/elevator_locator')
 
         if locator.isEmpty():
-            self.notify.warning(
-                'Could not find elevator_origin or elevator_locator; '
-                'parenting the elevator to render.'
-            )
             self.elevatorModel.reparentTo(render)
         else:
             self.elevatorModel.reparentTo(locator)
@@ -91,6 +100,192 @@ class DistributedPaceElevator(DistributedBossElevator.DistributedBossElevator):
             ),
             Func(self.onDoorCloseFinish)
         )
+
+    def onDoorCloseFinish(self):
+        return
+
+    def enterClosed(self, ts):
+        self.forceDoorsClosed()
+        self.startRide(ts)
+
+    def exitClosed(self):
+        pass
+
+    def startRide(self, ts=0):
+        self.stopRide()
+
+        base.musicManager.stopAllSounds()
+
+        place = self.cr.playGame.getPlace()
+        if place and hasattr(place, 'loader') and hasattr(place.loader, 'geom'):
+            self.rideHiddenGeom = place.loader.geom
+            self.rideHiddenGeom.hide()
+
+        for seatIndex in xrange(len(self.elevatorPoints)):
+            avId = 0
+            if seatIndex < len(self.boardedAvIds.keys()):
+                avId = self.boardedAvIds.keys()[seatIndex]
+
+            toon = self.cr.doId2do.get(avId)
+            if toon:
+                toon.reparentTo(self.elevatorModel)
+                toon.setPos(apply(Point3, self.elevatorPoints[seatIndex]))
+                toon.setHpr(180, 0, 0)
+                toon.setAnimState('neutral', 1.0)
+                toon.show()
+                toon.setShadowHeight(0)
+
+        self.rideRoot = render.attachNewNode(self.uniqueName('paceRideRoot'))
+        self.rideOriginalParent = self.elevatorModel.getParent()
+        self.rideOriginalMat = self.elevatorModel.getMat(render)
+
+        self.elevatorModel.reparentTo(self.rideRoot)
+        self.elevatorModel.setMat(render, self.rideOriginalMat)
+
+        camera.reparentTo(self.elevatorModel)
+        camera.setPos(0.0, 12.0, 4.6)
+        camera.setHpr(180, -6, 0)
+
+        self.rideElevatorStartPos = self.rideRoot.getPos()
+
+        self.rideMusic.setLoop(False)
+        self.rideMusic.setVolume(1.0)
+        self.rideMusic.play()
+
+        cameraMid = Point3(0.0, 12.0, 4.6)
+        cameraMin = Point3(0.0, 12.0, 4.4)
+        cameraMax = Point3(0.0, 12.0, 4.8)
+
+        self.rideTrack = Sequence(
+            Wait(0.5),
+
+            LerpPosInterval(camera,0.5,cameraMin,startPos=cameraMid,blendType='easeOut'),
+            LerpPosInterval(camera,0.5,cameraMid,startPos=cameraMin),
+
+            Wait(0.75),
+
+            LerpPosInterval(camera,0.5,cameraMax,startPos=cameraMid,blendType='easeOut'),
+            LerpPosInterval(camera,0.75,cameraMid,startPos=cameraMax),
+
+            Wait(0.75),
+
+            LerpPosInterval(camera,0.45,cameraMin,startPos=cameraMid,blendType='easeOut'),
+            LerpPosInterval(camera,0.45,cameraMid,startPos=cameraMin),
+
+            Wait(0.75),
+
+            LerpPosInterval(camera,0.45,cameraMax,startPos=cameraMid,blendType='easeOut'),
+            LerpPosInterval(camera,0.75,cameraMid,startPos=cameraMax),
+
+            Wait(0.9),
+            Func(self.finishRide)
+        )
+        self.rideTrack.start(ts)
+
+    def finishRide(self):
+        if self.rideElevatorStartPos is not None:
+            if self.rideRoot:
+                self.rideRoot.setPos(self.rideElevatorStartPos)
+            self.rideElevatorStartPos = None
+
+        if self.rideHiddenGeom:
+            self.rideHiddenGeom.show()
+            self.rideHiddenGeom = None
+
+    def stopRide(self):
+        if self.rideTrack:
+            self.rideTrack.pause()
+            self.rideTrack = None
+
+        if self.rideElevatorStartPos is not None:
+            if self.rideRoot:
+                self.rideRoot.setPos(self.rideElevatorStartPos)
+            self.rideElevatorStartPos = None
+
+        self.restoreRideScene()
+
+    def restoreRideScene(self):
+        if self.rideRoot:
+            for avId in self.boardedAvIds.keys():
+                toon = self.cr.doId2do.get(avId)
+                if toon:
+                    toon.wrtReparentTo(render)
+
+            if self.rideOriginalParent:
+                currentMat = self.elevatorModel.getMat(render)
+                self.elevatorModel.reparentTo(self.rideOriginalParent)
+                self.elevatorModel.setMat(render, currentMat)
+
+            self.rideRoot.removeNode()
+            self.rideRoot = None
+            self.rideOriginalParent = None
+            self.rideOriginalMat = None
+
+        if self.rideHiddenGeom:
+            self.rideHiddenGeom.show()
+            self.rideHiddenGeom = None
+
+
+    def enterOpening(self, ts):
+        if self.rideTrack:
+            self.rideTrack.pause()
+            self.rideTrack = None
+
+        if self.rideHiddenGeom:
+            self.rideHiddenGeom.show()
+            self.rideHiddenGeom = None
+
+        taskMgr.remove(self.uniqueName('paceRideCleanup'))
+        taskMgr.doMethodLater(
+            ElevatorData[self.type]['openTime'],
+            self.__finishRideCleanup,
+            self.uniqueName('paceRideCleanup')
+        )
+
+        DistributedBossElevator.DistributedBossElevator.enterOpening(self, ts)
+
+    def __stopPaceRideMusic(self, task):
+        return task.done
+
+    def __finishRideCleanup(self, task):
+        camera.wrtReparentTo(render)
+        self.restoreRideScene()
+        return task.done
+
+    def setBossOfficeZone(self, zoneId):
+        self.stopRide()
+        camera.wrtReparentTo(render)
+        DistributedBossElevator.DistributedBossElevator.setBossOfficeZone(
+            self,
+            zoneId
+        )
+
+    def setBossOfficeZoneForce(self, zoneId):
+        self.stopRide()
+        camera.wrtReparentTo(render)
+        DistributedBossElevator.DistributedBossElevator.setBossOfficeZoneForce(
+            self,
+            zoneId
+        )
+
+    def disable(self):
+        taskMgr.remove(self.uniqueName('paceRideCleanup'))
+        taskMgr.remove(self.uniqueName('stopPaceRideMusic'))
+        if self.rideMusic:
+            self.rideMusic.stop()
+        camera.wrtReparentTo(render)
+        self.stopRide()
+        DistributedBossElevator.DistributedBossElevator.disable(self)
+
+    def delete(self):
+        taskMgr.remove(self.uniqueName('paceRideCleanup'))
+        taskMgr.remove(self.uniqueName('stopPaceRideMusic'))
+        if self.rideMusic:
+            self.rideMusic.stop()
+        camera.wrtReparentTo(render)
+        self.stopRide()
+        self.rideMusic = None
+        DistributedBossElevator.DistributedBossElevator.delete(self)
 
     def getElevatorModel(self):
         return self.elevatorModel
--- toontown/building/DistributedPaceElevatorAI.py
+++ toontown/building/DistributedPaceElevatorAI.py
@@ -1,9 +1,65 @@
+from direct.task import Task
 from toontown.building.ElevatorConstants import *
 from toontown.building import DistributedBossElevatorAI
+from toontown.building import DistributedElevatorExtAI
 
-class DistributedPaceElevatorAI(DistributedBossElevatorAI.DistributedBossElevatorAI):
 
-    def __init__(self, air, bldg, zone, antiShuffle = 0, minLaff = 0):
-        DistributedBossElevatorAI.DistributedBossElevatorAI.__init__(self, air, bldg, zone, antiShuffle=antiShuffle, minLaff=0)
+class DistributedPaceElevatorAI(
+        DistributedBossElevatorAI.DistributedBossElevatorAI):
+
+    RideDuration = 8.0
+
+    def __init__(self, air, bldg, zone, antiShuffle=0, minLaff=0):
+        DistributedBossElevatorAI.DistributedBossElevatorAI.__init__(
+            self,
+            air,
+            bldg,
+            zone,
+            antiShuffle=antiShuffle,
+            minLaff=minLaff
+        )
+
         self.type = ELEVATOR_PACE
         self.countdownTime = ElevatorData[self.type]['countdown']
+
+    def elevatorClosed(self):
+        if self.countFullSeats() <= 0:
+            self.fsm.request('closed')
+            return
+
+        self.fsm.request('closed')
+
+        taskMgr.doMethodLater(
+            self.RideDuration,
+            self.finishPaceRide,
+            self.uniqueName('pace-ride')
+        )
+
+    def enterClosed(self):
+        DistributedElevatorExtAI.DistributedElevatorExtAI.enterClosed(self)
+
+    def finishPaceRide(self, task):
+        try:
+            bossZone = self.bldg.createBossOffice(self.seats)
+        except AttributeError:
+            self.notify.warning(
+                'Pace ride finished but the Pace destination is not ready.'
+            )
+            self.fsm.request('opening')
+            return Task.done
+
+        for seatIndex in xrange(len(self.seats)):
+            avId = self.seats[seatIndex]
+            if avId:
+                self.sendUpdateToAvatarId(
+                    avId,
+                    'setBossOfficeZone',
+                    [bossZone]
+                )
+                self.clearFullNow(seatIndex)
+
+        return Task.done
+
+    def delete(self):
+        taskMgr.remove(self.uniqueName('pace-ride'))
+        DistributedBossElevatorAI.DistributedBossElevatorAI.delete(self)
