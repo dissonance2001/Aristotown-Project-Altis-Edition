6a1344394dc700ddd2dc824de4a150b0dd78f39b
Reia <microwavex7@gmail.com>
2026-09-21 04:07:25 +0200

    Update mongo-win32.bat

--- scripts/mongo-win32.bat
+++ scripts/mongo-win32.bat
@@ -4,6 +4,6 @@ mode con: cols=60 lines=20
 
 cd /d "%~dp0"
 
-"%~dp0..\dependencies\astron\mongo\Server\3.6\bin\mongod.exe" --dbpath "%~dp0..\dependencies\astron\mongo\astrondb-3.6" --bind_ip 127.0.0.1 --port 27017
+"%~dp0..\dependencies\astron\mongo\Server\3.6\bin\mongod.exe" --dbpath "%~dp0..\dependencies\astron\databases\astrondb" --bind_ip 127.0.0.1 --port 27017
 
 pause
